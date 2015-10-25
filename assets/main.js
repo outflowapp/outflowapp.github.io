92cc053b3862babbec771e88cf6df99bd8427c74
kirkbyo <ozzie@kirkbyo.com>
2015-10-24 21:33:20 -0400

    Add Twitter Tracking and removed redundant script

--- assets/main.js
+++ assets/main.js
@@ -1,3 +1,7 @@
 function logDownloadButtonClick() {
    ga('send', 'event', 'iOS Download Button', 'visited');
 }
+
+function logTwitterVisited() {
+   ga('send', 'event', 'Twitter Button', 'visited');
+}
